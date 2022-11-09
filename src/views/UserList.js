81b6a09461903e833bc17b93a438f22f53c37b9c
_digsales <diogodobu@gmail.com>
2022-11-09 11:04:02 -0300

    Adding UserList

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,36 @@
+import { Avatar, ListItem } from "@rneui/themed";
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, FlatList } from "react-native";
+import users from "../data/users";
 
 export default (props) => {
-  return <Text style={styles.container}>UserList</Text>;
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem
+        key={user.id}
+        bottomDivider
+        onPress={() => props.navigation.navigate("UserForm")}
+      >
+        <Avatar rounded source={{ uri: user.avatarUrl }} />
+        <ListItem.Content>
+          <ListItem.Title style={{ fontWeight: "bold" }}>
+            {user.name}
+          </ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        keyExtractor={(user) => user.id.toString()}
+        data={users}
+        renderItem={getUserItem}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
