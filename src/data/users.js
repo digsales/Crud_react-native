08b82ffebc6f4ad04cae394f3e3c5b06e45e3e4b
_digsales <diogodobu@gmail.com>
2022-11-09 10:49:08 -0300

    Add inital Data

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: "Diogo Sales",
+    email: "diogodobu@gmail.com",
+    avatarUrl:
+      "https://cdn.pixabay.com/photo/2013/07/12/19/15/gangster-154425_960_720.png",
+  },
+  {
+    id: 2,
+    name: "João Silva",
+    email: "joaosilva@gmail.com",
+    avatarUrl:
+      "https://cdn.pixabay.com/photo/2016/09/01/08/25/smiley-1635465_960_720.png",
+  },
+  {
+    id: 3,
+    name: "Marcos Almeida",
+    email: "Malmeida@gmail.com",
+    avatarUrl:
+      "https://cdn.pixabay.com/photo/2013/07/12/16/34/vampire-151178_960_720.png",
+  },
+  {
+    id: 4,
+    name: "Gisele Costa",
+    email: "GilCosta@gmail.com",
+    avatarUrl:
+      "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png",
+  },
+];
