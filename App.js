d2eeaf0b132fff4b6480d73c19379c38f31e9348
_digsales <diogodobu@gmail.com>
2022-11-09 10:40:25 -0300

    Add StackNavigation

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import UserList from "./src/views/UserList";
+import UserForm from "./src/views/UserForm";
+import Ionicon from "@expo/vector-icons/Ionicons";
+import { Button } from "@rneui/themed";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="UserList"
+        screenOptions={screenOptions}
+      >
+        <Stack.Screen
+          name="UserList"
+          component={UserList}
+          options={({ navigation }) => {
+            return {
+              title: "Lista de Usuários",
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate("UserForm")}
+                  type="clear"
+                  icon={<Ionicon name="add" size={30} color="white" />}
+                />
+              ),
+            };
+          }}
+        />
+        <Stack.Screen
+          name="UserForm"
+          component={UserForm}
+          options={{
+            title: "Formulário de Usuários",
+            headerTitleAlign: "center",
+          }}
+        />
+      </Stack.Navigator>
+      <StatusBar style="light" />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: "#f4511e",
   },
-});
+  headerTintColor: "white",
+  // headerTitleAlign: "center",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
---
+++ src/views/UserForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+export default (props) => {
+  return <Text style={styles.container}>UserForm</Text>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
